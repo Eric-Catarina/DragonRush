b60dfd3efaea396fb2288f639ccbdcc55f5b8310
Eric-Catarina <orment.cp@gmail.com>
2023-09-14 16:00:34 -0300

    fix: Player Multiplying

--- Assets/Scripts/BuffManager.cs
+++ Assets/Scripts/BuffManager.cs
@@ -1,33 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class BuffManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
+    public GameObject playerFab;
+    public bool isFirst;
+    
+    
     void OnTriggerEnter (Collider other) {
         if (other.gameObject.tag == "Gate") {
             if (other.GetComponent<Gate>().isPositive) {
-                Debug.Log("Add a buff");
-                // Increase self scale by 1.2x
-                transform.localScale *= 1.2f;
 
+                if (isFirst)
+                {
+                    GameObject playerCopy = Instantiate(playerFab, transform.position, Quaternion.identity);
+                    playerCopy.GetComponent<BuffManager>().isFirst = false;
+                }
+                
             } else {
                 transform.localScale *= 0.8f;
 
                 Debug.Log("Add a debuff");
             }
+        }
+    }
+
+    private void SummonCopies(int amount)
+    {
+        for (int i = 0; i < amount; i++)
+        {
+            GameObject playerCopy = Instantiate(playerFab, transform.position, Quaternion.identity);
+            playerCopy.GetComponent<BuffManager>().isFirst = false;
         }
     }
 }
---
+++ Assets/Scripts/CharactersManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharactersManager : MonoBehaviour
+{
+    public GameObject playerFab, firstCharacter;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
