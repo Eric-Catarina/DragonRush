7e608549b0dd0fd1cd1e8ace7ebfadbb3c0aa584
Éric Catarina Parreiras <1460909@sga.pucminas.br>
2023-10-17 19:01:31 -0300

    fix: Levels Buttons

--- Assets/Elements/UIButtons/LevelsMenu.cs
+++ Assets/Elements/UIButtons/LevelsMenu.cs
@@ -9,7 +9,7 @@ public class LevelsMenu : MonoBehaviour
     public Button[] levelButtons;
     public void Awake(){
         AssignLevelButtons();
-        UpdateLevelsAcces();
+        UpdateLevelsAccess();
     }
 
     public void StartLevel(int levelId){
@@ -17,8 +17,8 @@ public class LevelsMenu : MonoBehaviour
         SceneManager.LoadSceneAsync(levelName);
     }
 
-    public void UpdateLevelsAcces(){
-        int unlockedLevelsAmount = PlayerPrefs.GetInt("LevelsAmount", 1);
+    public void UpdateLevelsAccess(){
+        int unlockedLevelsAmount = PlayerPrefs.GetInt("LevelsAmount", 2);
         for (int i = 0; i < levelButtons.Length; i++){
             if (i < unlockedLevelsAmount){
                 levelButtons[i].interactable = true;
@@ -32,7 +32,7 @@ public class LevelsMenu : MonoBehaviour
         int levelsAmount = levelButtonsGameObject.transform.childCount;
         levelButtons = new Button[levelsAmount];
         for (int i = 0; i < levelButtons.Length; i++){
-            levelButtons[i] = transform.GetChild(i).GetComponent<Button>();
+            levelButtons[i] = levelButtonsGameObject.transform.GetChild(i).GetComponent<Button>();
         }
     }
 }
